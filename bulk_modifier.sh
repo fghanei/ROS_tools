2e778c7e83a8555e408ce7e1a2bdadcc3ec54f24
Farshad Ghanei <farshadg@buffalo.edu>
2019-11-18 11:36:15 -0500

    updated bulk_modifier

--- bulk_modifier.sh
+++ bulk_modifier.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for i in {61..80}
+#first input to this is the original bag file
+#second input is the destination path
+if [ "$#" -ne "2" ]; then
+    echo "Enter input file and destination path: ./bulk.sh <input_bag> <destination>"
+    exit 1
+fi
+
+for i in {1..10}
 do
     ii=$(($i-1))
 	my_num='1'
@@ -8,6 +15,6 @@ do
 	do
 		my_num=${my_num}0
 	done
-	./bag_modifier.py -f rate_1.bag -p $my_num -o rate_$i.bag
+	./bag_modifier.py -f $1 -p $my_num -t 20.0 -o $2/rate_$i.bag
 
 done
